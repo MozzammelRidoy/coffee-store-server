b0fbd917e79a688d3b7e67eee487f313c7cc0e4f
Mozzammel Ridoy <mozzammelridoy5iii@gmail.com>
2024-05-20 15:14:13 +0600

    domain access

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://coffee-store-d4b6d.web.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
